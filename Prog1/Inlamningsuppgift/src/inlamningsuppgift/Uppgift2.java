a9bc2626215a4e3e29b765dfa71d12d5798141eb
Daniel Berglund <tastyworks@gmail.com>
2014-10-13 13:14:06 +0200

    Uppgift 2
    Kommenterad

--- Prog1/Inlamningsuppgift/src/inlamningsuppgift/Uppgift2.java
+++ Prog1/Inlamningsuppgift/src/inlamningsuppgift/Uppgift2.java
@@ -7,14 +7,22 @@ public class Uppgift2 {
     public static void main(String[] args) {
         
         String first_input;
-        first_input = JOptionPane.showInputDialog("What?");
+        first_input = JOptionPane.showInputDialog("What do you want?");
         
+        /*Jag väljer att skapa en sträng för varje inmatning och ger dem namnen beroende på vilken ordning de ligger i */ 
+        /*Jag valde även att använda JOptionPane för inmatningarna*/
         System.out.println(first_input);
+        
+        /*Sedan skrivs inmatningen ut*/
         
         String second_input;
         second_input = JOptionPane.showInputDialog("Anything else?");
         
+        /*Frågorna som jag ställer vid inmatningara är ingenting som behöver läggas märke till*/
+        
         System.out.println(first_input + " " + second_input);
+        
+        /*Efter varje inmatning så väljer jag att lägga på dem som resulterar i en lång text med alla inmatningar*/
         
         String third_input;
         third_input = JOptionPane.showInputDialog("And?");
